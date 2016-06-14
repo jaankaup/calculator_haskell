ed64b61f5bee5d22eb8352f031f34a5e272b746c
Janne Kauppinen <janne.a.kauppinen@student.jyu.fi>
2016-06-14 15:41:26 +0300

    Main.hs is modified. Added a parser type.

--- main.hs
+++ main.hs
@@ -2,8 +2,19 @@ module Main where
 
 import Data.List
 import Control.Applicative
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+
 
 sum' xs = foldl' (\x y -> x + y) 0 xs
 
 sumMaybes :: (Num a) => Maybe a -> Maybe a -> Maybe a
 sumMaybes x y = pure (\a b -> a + b) <*> x <*> y
+
+newtype Parser a = P (L8.ByteString -> Maybe(a,L8.ByteString))
+
+runParser :: Parser a -> L8.ByteString -> Maybe (a,L8.ByteString)
+runParser (P f) = f
+
+main :: IO ()
+main = do print "joopajoo"
